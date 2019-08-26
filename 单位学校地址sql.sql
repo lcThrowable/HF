98ccd722b93d62993fd6ed544a290de403817bb6
longchong <lc554528196@163.com>
2019-08-26 15:11:10 +0800

    Signed-off-by: longchong <lc554528196@163.com>

---
+++ 单位学校地址sql.sql
@@ -0,0 +1,3 @@
+alter table lcappnt modify (company varchar2(60));
+alter table lcinsured modify (lcinsuredcompany varchar2(60));
+alter table lcinsuredTwo modify (lcinsuredcompany varchar2(60));
---
+++ 单位学校地址sql生产.sql
@@ -0,0 +1,3 @@
+alter table ybtcnpa1.lcappnt modify (company varchar2(60));
+alter table ybtcnpa1.lcinsured modify (lcinsuredcompany varchar2(60));
+alter table ybtcnpa1.lcinsuredTwo modify (lcinsuredcompany varchar2(60));
